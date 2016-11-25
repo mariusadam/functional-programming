614dd0eecc847e0fd13e0a45fefeb14f05643610
Marius Adam <marius.adam@evozon.com>
2016-11-25 03:41:21 +0200

    Update sets_diff.pl

--- p1/sets_diff.pl
+++ p1/sets_diff.pl
@@ -2,13 +2,13 @@
 
 member_of([H | _], H):-!.
 member_of([H | T], E):-
-  H =\= E,
-  member_of(T, E).
+    H =\= E,
+    member_of(T, E).
 
 sets_diff([], _, []):-!.
 sets_diff([H | T], S2, [H | R]):-
-  not(member_of(S2, H)),
-  sets_diff(T, S2, R).  
+    not(member_of(S2, H)),
+    sets_diff(T, S2, R).  
 sets_diff([H | T], S2, R):-
-  member_of(S2, H),
-  sets_diff(T, S2, R).
+    member_of(S2, H),
+    sets_diff(T, S2, R).
