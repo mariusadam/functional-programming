51cf0335f6449ac14a27d87139590ed4ba4ef940
Marius Adam <marius.adam@evozon.com>
2016-11-25 03:48:02 +0200

    Changed something

--- p2/p14.pl
+++ p2/p14.pl
@@ -1,24 +1,63 @@
-reverse( [] , [] ) . 
-reverse[ [X] , [X] ) .  
-reverse( [X|Xs] , R ) :- 
-  reverse(Xs,T) ,        
-    append( T , [X] , R ).   
+internal_reverse([], Acc, Acc).
+internal_reverse([H | T], Acc, R):-
+	internal_reverse(T, [H | Acc], R).
 
-dif( [], _, [] ).
+reverse(List, Reversed):- internal_reverse(List, [], Reversed). 
 
-dif( [0 | T], Last, Res ):-
-	dif(T, 1, Res2),
-	Res = [9 | Res2].
+internal_dif([], [], 0, Acc, Acc).
 
+internal_dif([], [], -1, Acc, [ - | Acc]).
 
-dif( [H | T], Last, Res):-
-	H2 is H - Last,
-	dif(T, 0, Res2),
-	Res = [H2 | Res2].
+internal_dif([H | T], [], Carry, Acc, Result):-
+	H + Carry < 0,
+	Digit is 10 + H + Carry,
+	internal_dif(T, [], -1, [Digit | Acc], Result).
 
-pred(List, R):-
-	reverse(List, Reversed),
-	dif(Reversed, 1, R2),
-	reverse(R2, R).
+internal_dif([H | T], [], Carry, Acc, Result):-
+	H + Carry >= 0,
+	Digit is H + Carry,
+	internal_dif(T, [], 0, [Digit | Acc], Result).
 
+internal_dif([], [H | T], _, Acc, Result):-
+	internal_dif([H | T], [], -1, Acc, Result).
+
+internal_dif([H1 | T1], [H2 | T2], Carry, Acc, Result):-
+	D is H1 + Carry,
+	D >= H2,
+	Digit is H1 + Carry - H2,
+	internal_dif(T1, T2, 0, [Digit | Acc], Result).
+
+internal_dif([H1 | T1], [H2 | T2], Carry, Acc, Result):-
+	D is H1 + Carry,
+	D < H2,
+	Digit is 10 + H1 + Carry - H2,
+	internal_dif(T1, T2, -1, [Digit | Acc], Result).
 	
+
+eliminate_zero([], []).
+
+eliminate_zero([0 | T], Result):-
+	eliminate_zero(T, Result).
+
+eliminate_zero([H | T], Result):-
+	H =\= 0,
+	Result = [H | T].
+
+dif(L1, L2, Result):-
+	reverse(L1, L1F),
+	reverse(L2, L2F),
+	internal_dif(L1F, L2F, 0, [], R2),
+	eliminate_zero(R2, Result).
+
+pred_foreach([], []).
+
+pred_foreach([H | T], Result):-
+	number(H),
+	pred_foreach(T, R2),
+	Result = [H | R2].
+
+pred_foreach([H | T], Result):-
+	is_list(H),
+	dif(H, [1], H2),
+	pred_foreach(T, R2),
+	Result = [H2 | R2].
