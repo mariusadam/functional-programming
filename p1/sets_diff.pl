a2cbba990dd230da6d8533f3116815fed38a12c2
Marius Adam <marius.adam@evozon.com>
2016-11-25 03:37:57 +0200

    Create sets_diff.pl

---
+++ p1/sets_diff.pl
@@ -0,0 +1,14 @@
+% Write a predicate that returns the difference of 2 sets
+
+member_of([H | _], H):-!.
+member_of([H | T], E):-
+  H =\= E,
+  member_of(T, E).
+
+sets_diff([], _, []):-!.
+sets_diff([H | T], S2, [H | R]):-
+  not(member_of(S2, H)),
+  sets_diff(T, S2, R).  
+sets_diff([H | T], S2, R):-
+  member_of(S2, H),
+  sets_diff(T, S2, R).
